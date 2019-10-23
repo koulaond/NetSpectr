2ebaf426b836af22e481e6e56cfd2cf8a80c765a
Koulas <ondrej.koula@gmail.com>
2019-10-23 23:19:30 +0200

    website structure update is synchronized by lock

--- src/main/java/core/WebsiteStructureHandler.java
+++ src/main/java/core/WebsiteStructureHandler.java
@@ -5,39 +5,42 @@ import lombok.Getter;
 
 import java.net.URL;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Getter(AccessLevel.PACKAGE)
 public class WebsiteStructureHandler {
     private WebsiteStructure structure;
+    private ReentrantLock reentrantLock = new ReentrantLock();
 
     public WebsiteStructureHandler() {
         this.structure = new WebsiteStructure();
     }
 
-    public void updateStructure(WebPage webPage) {
-        Map<URL, WebPageNode> webPageNodes = structure.getWebPageNodes();
-        if (structure.getInitialNode() == null) {
-            // First node
-            WebPageNode node = new WebPageNode();
-            node.setWebPage(webPage);
-            node.setSourceUrl(webPage.getSourceUrl());
-            structure.setInitialNode(node);
-            webPageNodes.put(webPage.getSourceUrl(), node);
-            adjustOutcomeNodes(webPage, webPageNodes, node);
-        } else {
-            WebPageNode node = webPageNodes.get(webPage.getSourceUrl());
-            if (node == null) {
-                node = new WebPageNode();
+    void updateStructure(WebPage webPage) {
+        reentrantLock.lock();
+        try {
+            Map<URL, WebPageNode> webPageNodes = structure.getWebPageNodes();
+            if (structure.getInitialNode() == null) {
+                // First node
+                WebPageNode node = new WebPageNode();
+                node.setWebPage(webPage);
+                node.setSourceUrl(webPage.getSourceUrl());
+                structure.setInitialNode(node);
                 webPageNodes.put(webPage.getSourceUrl(), node);
+                adjustOutcomeNodes(webPage, webPageNodes, node);
+            } else {
+                WebPageNode node = webPageNodes.get(webPage.getSourceUrl());
+                if (node == null) {
+                    node = new WebPageNode();
+                    webPageNodes.put(webPage.getSourceUrl(), node);
+                }
+                node.setWebPage(webPage);
+                node.setSourceUrl(webPage.getSourceUrl());
+                adjustOutcomeNodes(webPage, webPageNodes, node);
             }
-            node.setWebPage(webPage);
-            node.setSourceUrl(webPage.getSourceUrl());
-            adjustOutcomeNodes(webPage, webPageNodes, node);
+        } finally {
+            reentrantLock.unlock();
         }
-    }
-
-    public boolean isInStructure(URL location) {
-        return structure.getWebPageNodes().get(location) != null;
     }
 
     private void adjustOutcomeNodes(WebPage webPage, Map<URL, WebPageNode> webPageNodes, WebPageNode sourceNode) {
